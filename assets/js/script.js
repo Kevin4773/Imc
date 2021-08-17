4b4ac43aeab9c1697fff58849fcf53befc2117ca
KEVIN ALVES DA SILVA <21193393@CFP127ALUNO.LOCAL>
2021-08-17 16:28:54 -0300

    Resolvendo outro bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const calcular = document.getElementById("calcular")
 
 // Função que será responsável pelo cálculo, declaração das variáveis restantes, e estrutura de decisão para mensagem e correções de bugs.
 function imc() {
-    
+
     // Declaração das variáveis restantes para execução da função. (Obs: colocar dentro dos laços if, pois elas não serão globais)
     const txtNome = document.getElementById("txtNome").value // Capturando o *valor* das caixas, usando o .value
     const txtAltura = document.getElementById("txtAltura").value
@@ -11,33 +11,41 @@ function imc() {
     const resultado = document.getElementById("resultado")
 
     if (txtNome !== '' && txtAltura !== '' && txtPeso !== '') {
-        if (txtNome == NaN || txtAltura !== NaN || txtPeso !== NaN) {
+        if (isNaN(txtNome)) {
             
-            resultado.textContent = 'Tipo de dado inválido'
-        
-        } else {
+            if (isNaN(txtPeso) && isNaN(txtAltura)) {
+                
+                return resultado.textContent = 'Tipo de dado inválido'
             
-            const valorIMC = (txtPeso / (txtAltura * txtAltura)).toFixed(1) // Execução do cálculo usando .toFixed() para controlar as casas deciamis.
-
-            let classificacao = '' // Variável global (let) que vai abrigar as mensagens complementares do IMC.
-
-            // Laço if para mensagem completa.
-            if (valorIMC < 18.5) {
-                classificacao = 'abaixo do peso'
-            } else if (valorIMC < 24.9) {
-                classificacao = 'com peso ideal. Parabéns!!!'
-            } else if (valorIMC < 29.9) {
-                classificacao = 'acima do peso'
-            } else if (valorIMC < 34.9) {
-                classificacao = 'com obesidade grau I'
-            } else if (valorIMC < 40) {
-                classificacao = 'com obesidade grau II'
             } else {
-                classificacao = 'com obesidade grau III. Cuidado!!!'
+                
+                const valorIMC = (txtPeso / (txtAltura * txtAltura)).toFixed(1) // Execução do cálculo usando .toFixed() para controlar as casas deciamis.
+
+                let classificacao = '' // Variável global (let) que vai abrigar as mensagens complementares do IMC.
+
+                // Laço if para mensagem completa.
+                if (valorIMC < 18.5) {
+                    classificacao = 'abaixo do peso'
+                } else if (valorIMC < 24.9) {
+                    classificacao = 'com peso ideal. Parabéns!!!'
+                } else if (valorIMC < 29.9) {
+                    classificacao = 'acima do peso'
+                } else if (valorIMC < 34.9) {
+                    classificacao = 'com obesidade grau I'
+                } else if (valorIMC < 40) {
+                    classificacao = 'com obesidade grau II'
+                } else {
+                    classificacao = 'com obesidade grau III. Cuidado!!!'
+                }
+
+                /* Mandando a mensagem atráves de uma <div> usando o .textContent */
+                resultado.textContent = `${txtNome} seu IMC é ${valorIMC} e voçê está ${classificacao}`
             }
 
-            resultado.textContent = `${txtNome} seu IMC é ${valorIMC} e voçê está ${classificacao}`
-            /* Mandando a mensagem atráves de uma <div> usando o .textContent */
+        } else {
+            
+            return resultado.textContent = 'Tipo de dado inválido';
+        
         }
 
     } else {
